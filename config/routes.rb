7a252ef8bdc2194174ce76f5b9a4c2a2cfd1ca12
Yann Beilliard <yann.beilliard@gmail.com>
2017-12-06 23:13:01 +0000

    added homepage and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  
+  # Create the root page from an HTML file
+  root 'home#index'
+  get 'home/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
